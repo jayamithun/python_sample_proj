bb20cf6135e3449b92e7206f00d288f8b47d4f57
jayamithun <92734222+jayamithun@users.noreply.github.com>
2023-07-27 14:13:39 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,256 +1,306 @@
-# logic.py to be
-# imported in the 2048.py file
+from flask import Flask, render_template, jsonify, request,redirect,flash, Markup,url_for, session
+from models import *
+from datetime import date
 
-# importing random package
-# for methods to generate random
-# numbers.
-import random
+folder_name="static"
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:password@localhost:5432/inventoryDB"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = b'hkahs3720/'
+db.init_app(app)
 
-# function to initialize game / grid
-# at the start
-def start_game():
-    # declaring an empty list then
-    # appending 4 list each with four
-    # elements as 0.
-    mat = []
-    for i in range(4):
-        mat.append([0] * 4)
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+@app.route("/logout")
+def logout():
+    return render_template("index.html")    
+    
+@app.route("/dashboard", methods=["POST"])
+def dashboard():
+    unam = request.form.get("username")
+    pwd = request.form.get("password")
+    
+    ulist=user_tbl.query.filter_by(userName=unam).first()
+    session['username']= unam
+    session['userid']= ulist.userID
+    session['utype']= ulist.userType
+    if ulist.pwd==pwd:
+       # uid=ulist.userid
+        return render_template("dashboard.html")
+    else:  
+        return render_template("index.html")
+    
 
-    # printing controls for user
-    print("Commands are as follows : ")
-    print("'W' or 'w' : Move Up")
-    print("'S' or 's' : Move Down")
-    print("'A' or 'a' : Move Left")
-    print("'D' or 'd' : Move Right")
+@app.route("/saveItemCategory", methods=["POST"])
+def saveItemCategory():
+    idesc = request.form.get("ItemCategoryDesc")
+    ItemCategor = ItemCategory(ItemCatDesc=idesc)
+    db.session.add(ItemCategor)  
+    db.session.commit()
+    text=idesc+'record Saved Successfully'
+    return render_template("dashboard.html", text=text)  
+    
+@app.route("/ItemsCategory")    
+def ItemsCategory():
+    catList=ItemCategory.query.all()
+    tabl='''<div class="form-group">
+            <table class="table table-bordered border-success" ><TH>
+            <TD>Item Category ID</TD>
+            <TD>Item Category Name</TD></TH>'''  
+    for itm in catList:
+        tabl=tabl+"<TR ><td></td><TD>"+str(itm.ItemCatID)+"</TD><TD>"+itm.ItemCatDesc+"</TD></TR>"
+    tabl=tabl+"</TABLE></div>"   
+    strg='''<form action="/saveItemCategory" method="post">
+    <div class="text-center"><h5>Add Item Category</h5></div><hr>
+<div class="mb-3">
+  <label for="ItemCategoryDesc" class="form-label">Item Category Description</label>
+  <input type="text" class="form-control" name="ItemCategoryDesc" placeholder="Enter Item Category Name">
+</div>
+<div class="col-12">
+    <button type="submit" class="btn  btn-success mb-3">Save Item Category</button>
+</div>
 
-    # calling the function to add
-    # a new 2 in grid after every step
-    add_new_2(mat)
-    return mat
+</FORM>
+<BR>
+'''+tabl
+    text= Markup(strg)
+    return render_template("dashboard.html", text=text)   
 
+@app.route("/saveItem", methods=["POST"])
+def saveItem():
+    icatID = int(request.form.get("ItemCatID"))
+    itmNam = request.form.get("ItemName")
+    AvAmt = int(request.form.get("AvailAmt"))
+    ItemMas = ItemMaster(ItemCatID=icatID, ItemDesc=itmNam,AvailAmt=AvAmt )
+    db.session.add(ItemMas)  
+    db.session.commit()
+    text=itmNam+' record Saved Successfully'
+    return render_template("dashboard.html", text=text) 
+    
+@app.route("/Items")    
+def Items():
+    itmList= ItemMaster.query.all()
+    tabl='''<div class="form-group">
+            <table class="table table-bordered border-success" ><TH>
+            <TD>Item ID</TD>
+            <TD>Item Category</TD>
+            <TD>Item Name</TD></TH>'''  
+    for itm in itmList:
+        tabl=tabl+"<TR ><td></td><TD>"+str(itm.ItemID)+"</TD><TD>"+itm.ItemDesc+"</TD><TD>"+itm.itemCat.ItemCatDesc+"</TD></TR>"
+    tabl=tabl+"</TABLE></div>"  
+    catList= ItemCategory.query.all()
+    dd='''<div class="form-group">
+            <select class="form-control" name="ItemCatID">'''
+    for cat in catList:
+        dd=dd+"<option value=\""+str(cat.ItemCatID)+"\">"+cat.ItemCatDesc +"</option>"
+    dd=dd+  '''</select>
+</div>'''  
+    
+    
+    strg='''<form action="/saveItem" method="post">
+<div class="text-center"><h5>Add Items</h5></div><hr>
+<div class="mb-3">
+  <label for="ItemCatID" class="form-label">Item Category </label>'''+dd+'''
+  
+</div>
+<div class="mb-3">
+  <label for="ItemName" class="form-label">Item Description</label>
+  <input type="text" class="form-control" name="ItemName" placeholder="Enter Item Name">
+</div>
+<div class="mb-3">
+  <label for="AvailAmt" class="form-label">Available Amount</label>
+  <input type="text" class="form-control" name="AvailAmt" placeholder="How many Items available?">
+</div>
+<div class="col-12">
+    <button type="submit" class="btn  btn-success mb-3">Save Item</button>
+</div>
+</FORM>
+<BR>
+'''+tabl
+    
+    text=Markup(strg)
+    return render_template("dashboard.html", text=text)
 
-# function to add a new 2 in
-# grid at any random empty cell
-def add_new_2(mat):
-    # choosing a random index for
-    # row and column.
-    r = random.randint(0, 3)
-    c = random.randint(0, 3)
+@app.route("/UpdateItems", methods=["POST"])
+def UpdateItems():
+    iID = int(request.form.get("ItemID"))
+    aDate = request.form.get("AddDate")
+    aQty = int(request.form.get("AddQty"))
+    vName = request.form.get("VendorName")
+    aItems = AddItem(AddDate=aDate, AddQty=aQty,VendorName=vName, ItemID=iID)
+    upVal = ItemMaster.query.filter_by(ItemID=iID).first()
+    newUnits= upVal.AvailAmt
+    upVal.AvailAmt = newUnits+aQty
+    db.session.add(aItems)  
+    db.session.commit()
+    text=str(aQty)+' '+upVal.ItemDesc+' Added Successfully'
+    return render_template("dashboard.html", text=text) 
 
-    # while loop will break as the
-    # random cell chosen will be empty
-    # (or contains zero)
-    while (mat[r] != 0):
-        r = random.randint(0, 3)
-        c = random.randint(0, 3)
-
-    # we will place a 2 at that empty
-    # random cell.
-    mat[r] = 2
-
-
-# function to get the current
-# state of game
-def get_current_state(mat):
-    # if any cell contains
-    # 2048 we have won
-    for i in range(4):
-        for j in range(4):
-            if (mat[i][j] == 2048):
-                return 'WON'
-
-    # if we are still left with
-    # atleast one empty cell
-    # game is not yet over
-    for i in range(4):
-        for j in range(4):
-            if (mat[i][j] == 0):
-                return 'GAME NOT OVER'
-
-    # or if no cell is empty now
-    # but if after any move left, right,
-    # up or down, if any two cells
-    # gets merged and create an empty
-    # cell then also game is not yet over
-    for i in range(3):
-        for j in range(3):
-            if (mat[i][j] == mat[i + 1][j] or mat[i][j] == mat[i][j + 1]):
-                return 'GAME NOT OVER'
-
-    for j in range(3):
-        if (mat[3][j] == mat[3][j + 1]):
-            return 'GAME NOT OVER'
-
-    for i in range(3):
-        if (mat[i][3] == mat[i + 1][3]):
-            return 'GAME NOT OVER'
-
-    # else we have lost the game
-    return 'LOST'
-
-
-# all the functions defined below
-# are for left swap initially.
-
-# function to compress the grid
-# after every step before and
-# after merging cells.
-def compress(mat):
-    # bool variable to determine
-    # any change happened or not
-    changed = False
-
-    # empty grid
-    new_mat = []
-
-    # with all cells empty
-    for i in range(4):
-        new_mat.append([0] * 4)
-
-    # here we will shift entries
-    # of each cell to it's extreme
-    # left row by row
-    # loop to traverse rows
-    for i in range(4):
-        pos = 0
-
-        # loop to traverse each column
-        # in respective row
-        for j in range(4):
-            if (mat[i][j] != 0):
-
-                # if cell is non empty then
-                # we will shift it's number to
-                # previous empty cell in that row
-                # denoted by pos variable
-                new_mat[i][pos] = mat[i][j]
-
-                if (j != pos):
-                    changed = True
-                pos += 1
-
-    # returning new compressed matrix
-    # and the flag variable.
-    return new_mat, changed
-
-
-# function to merge the cells
-# in matrix after compressing
-def merge(mat):
-    changed = False
-
-    for i in range(4):
-        for j in range(3):
-
-            # if current cell has same value as
-            # next cell in the row and they
-            # are non empty then
-            if (mat[i][j] == mat[i][j + 1] and mat[i][j] != 0):
-                # double current cell value and
-                # empty the next cell
-                mat[i][j] = mat[i][j] * 2
-                mat[i][j + 1] = 0
-
-                # make bool variable True indicating
-                # the new grid after merging is
-                # different.
-                changed = True
-
-    return mat, changed
-
-
-# function to reverse the matrix
-# means reversing the content of
-# each row (reversing the sequence)
-def reverse(mat):
-    new_mat = []
-    for i in range(4):
-        new_mat.append([])
-        for j in range(4):
-            new_mat[i].append(mat[i][3 - j])
-    return new_mat
-
-
-# function to get the transpose
-# of matrix means interchanging
-# rows and column
-def transpose(mat):
-    new_mat = []
-    for i in range(4):
-        new_mat.append([])
-        for j in range(4):
-            new_mat[i].append(mat[j][i])
-    return new_mat
-
-
-# function to update the matrix
-# if we move / swipe left
-def move_left(grid):
-    # first compress the grid
-    new_grid, changed1 = compress(grid)
-
-    # then merge the cells.
-    new_grid, changed2 = merge(new_grid)
-
-    changed = changed1 or changed2
-
-    # again compress after merging.
-    new_grid, temp = compress(new_grid)
-
-    # return new matrix and bool changed
-    # telling whether the grid is same
-    # or different
-    return new_grid, changed
+@app.route("/AddItems")    
+def AddItems():
+    itmList= ItemMaster.query.all()
+    tabl='''<div class="form-group">
+            <table class="table table-bordered border-success" ><TH>
+            <TD>Item ID</TD>
+            <TD>Item Category</TD>
+            <TD>Item Name</TD>
+            <TD>Items Available</TD></TH>'''  
+    for itm in itmList:
+        tabl=tabl+"<TR ><td></td><TD>"+str(itm.ItemID)+"</TD><TD>"+itm.ItemDesc+"</TD><TD>"+itm.itemCat.ItemCatDesc+"</TD><TD>"+str(itm.AvailAmt)+"</TD></TR>"
+    tabl=tabl+"</TABLE></div>"  
+    dd='''<div class="form-group">
+            <select class="form-control" name="ItemID">'''
+    for itm in itmList:
+        dd=dd+"<option value=\""+str(itm.ItemID)+"\">"+itm.ItemDesc +"</option>"
+    dd=dd+  '''</select>
+</div>''' 
+    strg='''<form action="/UpdateItems" method="post">
+<div class="text-center"><h5>Items Available</h5></div><hr>
+<div class="mb-3">
+  <label for="ItemID" class="form-label">Select Item </label>'''+dd+'''
+</div>
+<div class="mb-3">
+  <label for="AddDate" class="form-label">Select Date</label>
+  <input type="date" class="form-control" Name="AddDate" placeholder="Enter Date of Adding Items in Inventory">
+</div>
+<div class="mb-3">
+  <label for="AddQty" class="form-label">Items Count</label>
+  <input type="text" class="form-control" name="AddQty" placeholder="Items Count">
+</div>
+<div class="mb-3">
+  <label for="VendorName" class="form-label">Vendor Name</label>
+  <input type="text" class="form-control" name="VendorName" placeholder="Vendor Name">
+</div>
+<div class="col-12">
+    <button type="submit" class="btn btn-success mb-3">Save Item</button>
+</div>
+</FORM><BR>
+'''+tabl
+    text=Markup(strg)
+    return render_template("dashboard.html", text=text)  
 
 
-# function to update the matrix
-# if we move / swipe right
-def move_right(grid):
-    # to move right we just reverse
-    # the matrix
-    new_grid = reverse(grid)
+@app.route("/issUpdt/<int:tid>")
+def issUpdt(tid):
+    
+    ilist=request_tbl.query.filter_by(reqID=tid).first()
+    upVal="I"
+    ilist.Status=upVal
+    ilist.issueDate= date.today()
+    imlist=ItemMaster.query.filter_by(ItemID=ilist.ItemID).first()
+    currCnt=imlist.AvailAmt
+    imlist.AvailAmt=currCnt-ilist.ReqQty
+    db.session.commit()
+    text="Item Issued"
+    return render_template("dashboard.html", text=text)
 
-    # then move left
-    new_grid, changed = move_left(new_grid)
+@app.route("/itemIssue")    
+def itemIssue():
+    itmList= request_tbl.query.filter_by(Status=None,)
+    strg='''<Form><div class="form-group">
+            <table class="table table-bordered border-success" ><TH>
+            <TD>Item Name</TD>
+            <TD>Date of Request</TD>
+            <TD>Quantity Requested</TD>
+            <TD>User Name</TD>
+            <TD>Issue</TD></TH>'''  
+    for itm in itmList:
+        strg=strg+"<TR ><td></td><TD>"+str(itm.item.ItemDesc)+"</TD><TD>"+str(itm.ReqDate)+"</TD><TD>"+str(itm.ReqQty)+"</TD>"
+        strg=strg+"<TD>"+str(itm.user.userName)+"</TD><TD>"
+        strg=strg+"<div class=\"col-12\"><a href="+ url_for('issUpdt',tid=itm.reqID)+">Issue</a></div></TD></TR>"
+    strg=strg+"</TABLE></div></FORM>"
+    
+    text=Markup(strg)
+    return render_template("dashboard.html", text=text) 
+  
+@app.route("/retUpdt/<int:tid>")
+def retUpdt(tid):
+    ilist=request_tbl.query.filter_by(reqID=tid).first()
+    upVal="R"
+    ilist.Status=upVal
+    ilist.returnDate= date.today()
+    imlist=ItemMaster.query.filter_by(ItemID=ilist.ItemID).first()
+    currCnt=imlist.AvailAmt
+    imlist.AvailAmt=currCnt+ilist.ReqQty
+    db.session.commit()
+    text="Item Returned"
+    return render_template("dashboard.html", text=text)
+    
+@app.route("/itemReturn")    
+def itemReturn():
+    itmList= request_tbl.query.filter_by(Status="I",userID=session['userid'])
+    strg='''<Form><div class="form-group">
+            <table class="table table-bordered border-success" ><TH>
+            <TD>Item Name</TD>
+            <TD>Date of Issue</TD>
+            <TD>Quantity Issued</TD>
+            <TD>Return</TD></TH>'''  
+    for itm in itmList:
+        strg=strg+"<TR ><td></td><TD>"+str(itm.item.ItemDesc)+"</TD><TD>"+str(itm.issueDate)+"</TD><TD>"+str(itm.ReqQty)+"</TD>"
+        strg=strg+"<TD><div class=\"col-12\"><a href="+ url_for('retUpdt',tid=itm.reqID)+">Return</a></div></TD></TR>"
+    strg=strg+"</TABLE></div></FORM>"
+    
+    text=Markup(strg)
+    return render_template("dashboard.html", text=text)      
+    
+@app.route("/saveRequest", methods=["POST"])
+def saveRequest():
+    iID = int(request.form.get("ItemID"))
+    rDate = request.form.get("reqDate")
+    rQty = request.form.get("reqQuantity")
+    uid = session["userid"]
+    dept = request.form.get("depttName")
+    rItems = request_tbl(ReqDeptt=dept, ReqDate=rDate,ItemID=iID, ReqQty=rQty, userID=uid)
+    db.session.add(rItems)  
+    db.session.commit()
+    text=str(iID)+' '+str(rQty)+' Requested Successfully'
+    return render_template("dashboard.html", text=text) 
 
-    # then again reverse matrix will
-    # give us desired result
-    new_grid = reverse(new_grid)
-    return new_grid, changed
 
+@app.route("/itemRequest")    
+def itemRequest():
+    itmList= ItemMaster.query.all()
+    dd='''<div class="form-group">
+            <select class="form-control" name="ItemID">'''
+    for itm in itmList:
+        dd=dd+"<option value=\""+str(itm.ItemID)+"\">"+itm.ItemDesc +"</option>"
+    dd=dd+  '''</select>
+</div>''' 
 
-# function to update the matrix
-# if we move / swipe up
-def move_up(grid):
-    # to move up we just take
-    # transpose of matrix
-    new_grid = transpose(grid)
-
-    # then move left (calling all
-    # included functions) then
-    new_grid, changed = move_left(new_grid)
-
-    # again take transpose will give
-    # desired results
-    new_grid = transpose(new_grid)
-    return new_grid, changed
-
-
-# function to update the matrix
-# if we move / swipe down
-def move_down(grid):
-    # to move down we take transpose
-    new_grid = transpose(grid)
-
-    # move right and then again
-    new_grid, changed = move_right(new_grid)
-
-    # take transpose will give desired
-    # results.
-    new_grid = transpose(new_grid)
-    return new_grid, changed
-
-# this file only contains all the logic
-# functions to be called in main function
-# present in the other file
+    itmList= request_tbl.query.filter_by(userID=session['userid'])
+    tbl='''<DIV><table class="table table-bordered border-success" ><TH>
+            <TD>Item Name</TD>
+            <TD>Date of Request</TD>
+            <TD>Date of Issue</TD>
+            <TD>Quantity Issued</TD></TH>'''  
+    for itm in itmList:
+        tbl=tbl+"<TR ><td></td><TD>"+str(itm.item.ItemDesc)+"</TD><TD>"+str(itm.ReqDate)+"</TD><TD>"+str(itm.issueDate)+"</TD>"
+        tbl=tbl+"<TD>"+str(itm.ReqQty)+"</TD></TR>"
+    tbl=tbl+"</TABLE></div>"    
+    strg='''<form action="/saveRequest" method="post">
+<div class="text-center"><h5>Request Item</h5></div><hr>
+<div class="mb-3">
+  <label for="ItemID" class="form-label">Select Item </label>'''+dd+'''
+</div>
+<div class="mb-3">
+  <label for="reqDate" class="form-label">Request Date</label>
+  <input type="date" class="form-control" name="reqDate" placeholder="Select Request Date">
+</div>
+<div class="mb-3">
+  <label for="reqQty" class="form-label">Request Quantity</label>
+  <input type="number" class="form-control" name="reqQuantity" placeholder="Items Count">
+</div>
+<div class="mb-3">
+  <label for="depttName" class="form-label">Department</label>
+  <input type="text" class="form-control" name="depttName" placeholder="Department ">
+</div>
+<div class="col-12">
+    <button type="submit" class="btn btn-success mb-3">Request Item</button>
+</div>
+</FORM><BR>
+'''+tbl
+    text=Markup(strg)
+    return render_template("dashboard.html", text=text)  
